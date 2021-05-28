ffcaf1b10f445f604dc16af90805d5508c760a47
YagyaMittal <yagyamittal02@gmail.com>
2021-05-28 16:17:49 +0530

    uploading

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
+import facebook from './screens/facebook';
+import insta from './screens/insta';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <AppContainer />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: 'cyan',
+    justifyContent: 'center'
+  }
 });
+
+const TabNavigator = createBottomTabNavigator({
+   facebook: {screen: facebook},
+   insta: {screen: insta}
+});
+
+const AppContainer = createAppContainer(TabNavigator);
